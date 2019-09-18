1800cab6a1e78a35bd8ca0f267e0c402b4e61cc0
pelissierk <pelissierk@K131-9.dep-info.iutmontp.univ-montp2.fr>
2019-09-18 10:38:04 +0200

    fin du test de la classe lirevoiture

--- TD2/Conf.php
+++ TD2/Conf.php
@@ -7,7 +7,7 @@ class Conf {
     'hostname' => 'webinfo',
     // A l'IUT, vous avez une BDD nommee comme votre login
     // Sur votre machine, vous devrez creer une BDD
-    'database' => 'voiture',
+    'database' => 'pelissierk',
     // A l'IUT, c'est votre login
     // Sur votre machine, vous avez surement un compte 'root'
     'login' => 'pelissierk',
--- TD2/lireVoiture.php
+++ TD2/lireVoiture.php
@@ -3,4 +3,9 @@ require_once "Model.php";
 
 $rep = Model::$pdo->query("SELECT * FROM voiture");
 $tab_obj = $rep->fetchAll(PDO::FETCH_OBJ);
+
+foreach ($tab_obj as $key => $value) {
+	echo "immatriculation => {$value->immatriculation} <br> couleur => {$value->couleur} <br> marque => {$value->marque} <br><br>";
+}
+
 ?>

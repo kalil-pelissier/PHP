7fdcc456fab8b9a3456d10f0f491635c7b75be7c
kalil <kalil.pelissier@etu.umontpellier.fr>
2019-10-07 00:29:51 -0400

    correction

--- TD5/controller/routeur.php
+++ TD5/controller/routeur.php
@@ -1,6 +1,6 @@
 <?php
 require_once'/wamp64/www/PHP/TD5/lib/File.php';
-require_once 'ControllerVoiture.php';
+require_once'ControllerVoiture.php';
 // On recupère l'action passée dans l'URL
 $action = $_GET['action'];
 ControllerVoiture::$action(); 

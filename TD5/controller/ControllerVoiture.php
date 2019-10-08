c78484dc03e2c8cddb2ec1c780ddd857a4880e1a
kalil <kalil.pelissier@etu.umontpellier.fr>
2019-10-07 23:26:09 -0400

    changement chemin absolue de controllerVoiture

--- TD5/controller/ControllerVoiture.php
+++ TD5/controller/ControllerVoiture.php
@@ -1,5 +1,5 @@
 <?php
-require_once ('../model/ModelVoiture.php'); // chargement du modèle
+require_once File::build_path(array('model','ModelVoiture.php')); 
 
 class ControllerVoiture {
     public static function readAll() {

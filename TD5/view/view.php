95005ca3d22b9b336bb3cd68e538730a0d674ad2
kalil <kalil.pelissier@etu.umontpellier.fr>
2019-10-15 15:47:54 -0400

    to see

--- TD5/view/view.php
+++ TD5/view/view.php
@@ -6,17 +6,19 @@
 </head>
 <body>
 
-	<header> 
-			<p style="border: 1px solid black;text-align:left;padding-left:1em;">
-				<a href="index.php?action=readAll"> Liste des voitures</a>
-			  </p>
-				<p style="border: 1px solid black;text-align:left;padding-left:1em;">
-				<a href="index.php?action=readAll&controller=utilisateur"> Utilisateur</a>
-			</p>
-			<p style="border: 1px solid black;text-align:left;padding-left:1em;">
-				<a href="index.php?action=readAll&controller=trajet"> Trajet</a>
-			</p>
-	</header>
+<header> 
+	<p style="border: 1px solid black;text-align:left;padding-left:1em;">
+		<a href="index.php?action=readAll"> Liste des voitures</a>
+	</p>
+
+	<p style="border: 1px solid black;text-align:left;padding-left:1em;">
+		<a href="index.php?action=readAll&controller=utilisateur"> Utilisateur</a>
+	</p>
+
+	<p style="border: 1px solid black;text-align:left;padding-left:1em;">
+		<a href="index.php?action=readAll&controller=trajet"> Trajet</a>
+	</p>
+</header>
 
 	<?php
 // Si $controleur='voiture' et $view='list',

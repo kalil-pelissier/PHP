0e2b45c45536eff6ccb264fdba8a500f9ef68139
kalil <kalil.pelissier@etu.umontpellier.fr>
2019-10-29 19:26:09 -0400

    ajout d'une nouvelle page erreur

---
+++ TD5/view/voiture/rongArg.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Detaiil sur ma voitures</title>
+    </head>
+    <body>
+        <?php   
+         echo 'Cette action est impossible'
+        ?>
+    </body>
+</html>

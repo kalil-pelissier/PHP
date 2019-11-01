ab96471587e0af6216aeddfe412a79ce9520165f
kalil <kalil.pelissier@etu.umontpellier.fr>
2019-10-31 22:46:26 -0400

    gestion de plusieurs controlleurs

--- TD5/controller/routeur.php
+++ TD5/controller/routeur.php
@@ -4,17 +4,26 @@ require_once File::build_path(array('controller','ControllerVoiture.php'));
 if(!isset($_GET['action'])) {
 	$_GET['action'] = 'readAll';
 }
- 
-// On recupère l'action passée dans l'URL
-$action = $_GET['action'];
-//On récupère toutes les méthodes de la classe ControllerVoiture
-$methodes = get_class_methods ('ControllerVoiture');
 
-//vérification si la methode passer existe bien
-if(!in_array($_GET['action'], $methodes)) {
-	ControllerVoiture::error();
+if(!isset($_GET['controller'])) {
+	$_GET['controller'] = 'voiture';
+}
+ 
+// On recupère l'action et le controleur passée dans l'URL
+$action = $_GET['action'];
+$controller = $_GET['controller'];
+
+//création du nom de classe
+$controller_class = 'Controller'.ucfirst($controller);
+
+//On récupère toutes les méthodes de la classe ControllerVoiture
+$methodes = get_class_methods ($controller_class);
+
+//vérification si la class et l'action existe bien
+if(class_exists($controller_class) && in_array($_GET['action'], $methodes)) {
+	$controller_class::$action();
 }else {
-	ControllerVoiture::$action();
+	ControllerVoiture::error();
 }
 
 ?>
